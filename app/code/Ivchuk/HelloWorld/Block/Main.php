bb713b5c5baab4c43e1107c8222bd59fd6695331
SerhiiIvchuk <sergii.i@open-tag.com>
2021-10-22 23:00:01 +0300

    2.1-no-mvc:View/View Model

--- app/code/Ivchuk/HelloWorld/Block/Main.php
+++ app/code/Ivchuk/HelloWorld/Block/Main.php
@@ -9,6 +9,10 @@ class Main extends Template
 {
     protected function _prepareLayout()
     {
-
+        $this->setMessage('Hello Again World');
+    }
+    public function getGoodbyeMessage()
+    {
+        return 'Goodbye World';
     }
 }
