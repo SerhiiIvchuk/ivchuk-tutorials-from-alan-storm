054d497e62611ecb6a71d82fd8de01a9ab95d55d
SerhiiIvchuk <sergii.i@open-tag.com>
2021-10-22 22:48:29 +0300

    2.1-no-mvc:block+layout

--- app/code/Ivchuk/HelloWorld/Controller/Hello/World.php
+++ app/code/Ivchuk/HelloWorld/Controller/Hello/World.php
@@ -3,12 +3,23 @@ declare(strict_types=1);
 
 namespace Ivchuk\HelloWorld\Controller\Hello;
 
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\App\Action\Context;
+
 class World extends \Magento\Framework\App\Action\Action
 {
+    protected $pageFactory;
+
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+        return parent::__construct($context);
+        $this->context = $context;
+    }
     public function execute()
     {
-        echo '<p>You Did It!</p>';
-//        var_dump(__METHOD__);
+        var_dump(__METHOD__);
+        $page_object = $this->pageFactory->create();;
+        return $page_object;
     }
 }
-
